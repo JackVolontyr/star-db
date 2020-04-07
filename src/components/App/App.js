af3cb6b3fa83d3df1f9c248954f447c76ce52e4f
JackVolontyr <soapforwork@gmail.com>
2020-04-07 23:22:58 +0300

    play with the hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment, useEffect, useState, useCallback, useMemo } from 'react';
 
 import Header from '../Header';
 import { PagePerson, PagePlanet, PageStarship, } from '../Page_s';
@@ -13,7 +13,7 @@ import './App.css';
 
 const swapiService = new SwapiService();
 
-const App = () => <div className="container">
+const _App = () => <div className="container">
   <ErrorCatcher className="sw-block sw-block--padding jumbotron rounded">
     <ProviderSwapiService value={ swapiService }>
       <Header />
@@ -26,5 +26,108 @@ const App = () => <div className="container">
     </ProviderSwapiService>
   </ErrorCatcher>
 </div>
+
+/* v Test v */
+const Notification = () => {
+
+  const [ visible, setVisible ] = useState(true);
+
+  useEffect(
+    () => {
+      console.log('effect');
+
+      const timeout = setTimeout(
+        () => setVisible(false), 
+        2000
+      );
+
+      return () => clearTimeout(timeout);
+    },
+    []
+  );
+
+  return visible && <div style={ { padding: `10px` } }>
+    Hi, I am Notification!
+  </div>
+}
+
+const getPlanet = (id) => fetch(`https://swapi.co/api/planets/${id}`)
+    .then(res => res.json())
+    .then(data => data);
+
+const useRequest = (request) => {
+  const inititalState = useMemo(() => ({
+    data: null,
+    loading: true,
+    error: null,
+  }), []);
+
+  const [dataState, setDataState] = useState(inititalState);
+
+  useEffect(() => {
+    setDataState(inititalState);
+
+    let isReady = true;
+    request()
+      .then(data => isReady && setDataState({
+        data,
+        loading: false,
+        error: null,
+      }))
+      .catch(error => isReady && setDataState({
+        data: null,
+        loading: false,
+        error,
+      }));
+
+    return () => isReady = false;
+  }, [request, inititalState]);
+
+  return dataState;
+} 
+
+const usePlanetInfo = (id) => {
+  const request = useCallback(() => getPlanet(id), [ id ]);
+  return useRequest(request);
+} 
+
+const PlanetInfo = () => {
+  const [ id, setId ] = useState(1);
+  const [ visible, setVisible ] = useState(true);
+  
+  const { data, loading, error } = usePlanetInfo(id);
+
+  const buttons = <Fragment>
+    <button type="button" onClick={() => setId((id) => id - 1)}>-</button>
+    <button type="button" onClick={() => setId((id) => id + 1)}>+</button>
+    <button type="button" onClick={() => setVisible(false)}>hide</button>
+  </Fragment>;
+
+  if (visible) {
+    if (error) {
+      return <div style={{ padding: `20px` }}>{ buttons } 
+        <div>Error!</div>
+      </div>
+    
+    } else if (loading) {
+      return <div style={{ padding: `20px` }}>{buttons}
+        <div>Loading...</div>
+      </div>
+
+    } else if (data) {
+      return <div style={{ padding: `20px` }}>
+        { buttons }
+        <div>{id} - {data && data.name}</div>
+      </div>
+    }
+  } else {
+    return <button style={{ margin: `20px` }} type="button" onClick={() => { setVisible(true); setId(1); } }>show</button>
+  }
+}
+
+const App = () => <div>
+  <PlanetInfo />
+</div>
+/* ^ Test ^ */
 
 export default App;
