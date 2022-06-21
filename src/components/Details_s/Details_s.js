36ec5c75cbe5f67a7e58828fbc713feffcf35e5c
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-21 14:37:08 +0300

    Detail_s cmpnt: +plug if is not a Swapi

--- src/components/Details_s/Details_s.js
+++ src/components/Details_s/Details_s.js
@@ -5,37 +5,42 @@ import Spinner from '../Spinner';
 import ItemField from '../ItemField';
 import { HocConsumerSwapi } from '../HocHelpers';
 
-
 const DetailsPlanetRandom = HocConsumerSwapi(
-  ({ props, swapiService }) => {
-    const { getPlanet, getImage } = swapiService;
-    // const itemId = Math.floor(Math.random() * 25) + 2;
-    const itemId = 1;
+	({ isSwapiResult, swapiService }) => {
+		const { getPlanet, getImage } = swapiService;
+		const itemId = Math.floor(Math.random() * 25) + 2;
 
-    return <Fragment>
-      <h2>Welcome to StarDB!</h2>
-      <Details
-        dataPromise={getPlanet(itemId)}
-        imageString={'planets'}
-        imageData={getImage}
-        defaultMessage={<Spinner />}
-        itemId={itemId}>
-        <ItemField field="population" label="Population" />
-        <ItemField field="rotationPeriod" label="Rotation Period" />
-        <ItemField field="diameter" label="Diameter" />
-      </Details>
-    </Fragment>
-  }
+		return isSwapiResult 
+		? 
+		<Fragment>
+			<h2>Welcome to StarDB!</h2>
+			<Details
+				dataPromise={getPlanet(itemId)}
+				imageString={'planets'}
+				imageData={getImage}
+				defaultMessage={<Spinner />}
+				itemId={itemId}>
+				<ItemField field="population" label="Population" />
+				<ItemField field="rotationPeriod" label="Rotation Period" />
+				<ItemField field="diameter" label="Diameter" />
+			</Details>
+		</Fragment> 
+		: 
+		<div className="sw-block jumbotron rounded">
+			<div className="sw-block--padding rounded">
+				<span>Set Swapi service in the header to see actual data, please.</span>
+			</div>
+		</div>
+	}
 );
-
 
 const DetailsPerson = () => {}
 const DetailsPlanet = () => {}
 const DetailsStarship = () => {}
 
 export { 
-  DetailsPlanetRandom, 
-  DetailsPerson, 
-  DetailsPlanet, 
-  DetailsStarship, 
+	DetailsPlanetRandom, 
+	DetailsPerson, 
+	DetailsPlanet, 
+	DetailsStarship, 
 };
