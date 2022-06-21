62f9276938a650f5660a13868c96fecaffd0d320
Yevhen Volontyr <soapforwork@gmail.com>
2022-06-21 14:38:23 +0300

    Header: +current service info, +webRoot to Links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { webRoot } from '../../utils';
 
 import './Header.css';
 
-const Header = ({ changeService }) => {
-  return (
-    <div className="d-flex sw-header">
-      <h3><Link to="/">StarDB</Link></h3>
-      <ul className="d-flex sw-nav">
-        <li><Link to="/people/">People</Link></li>
-        <li><Link to="/planets/">Planets</Link></li>
-        <li><Link to="/starships/">Starships</Link></li>
-      </ul>
-      <button
-        onClick={changeService} 
-        className="btn btn-primary btn-sm"
-      >
-        Change Service
-      </button>
-    </div>
-  );
+const Header = ({ changeService, serviceTextInfo }) => {
+	return (
+		<div className="d-flex sw-header">
+			<h3><Link to={`${webRoot}/`}>StarDB</Link></h3>
+			<ul className="d-flex sw-nav">
+				<li><Link to={`${webRoot}/people/`}>People</Link></li>
+				<li><Link to={`${webRoot}/planets/`}>Planets</Link></li>
+				<li><Link to={`${webRoot}/starships/`}>Starships</Link></li>
+			</ul>
+			<button
+				onClick={changeService} 
+				className="btn btn-primary btn-sm"
+			>
+				Change Service (current: {serviceTextInfo})
+			</button>
+		</div>
+	);
 }
 
 export default Header;
